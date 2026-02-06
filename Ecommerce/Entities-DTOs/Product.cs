373aa84e3dfb70fac5d301f154c78ecbf7d3b266
DINIA LOPEZ NUÑEZ <DINIA LOPEZ NUEZ@DESKTOP-F6MQNOU>
2026-02-05 19:28:37 -0600

    inclusion del proyecto de entidades

---
+++ Ecommerce/Entities-DTOs/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Entities_DTOs
+{
+    public class Product : BaseDTO
+    {
+        public string Name { get; set; }
+        public string Category { get; set; } 
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+
+
+    }
+}
