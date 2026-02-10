8a4003f4324b07f781e9cd1cc4360d24cdcdc630
Dario Chaves <DINIA LOPEZ@DESKTOP-OAPB7LG>
2026-02-10 12:05:08 -0600

    clases sql

---
+++ Ecommerce/DataAccess/DAO/sqlDAO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace DataAccess.DAO
+{   
+    //Vamos usar el patron del singleton
+    /*
+     * Clase que se encarga de la comunicacion con la base de datos
+     * Asegura que solo exista una unica instancia de la clase 
+     */
+    public class sqlDAO
+    {
+        //Paso 1: Crear una instancia privada de la misma clase
+        private static sqlDAO instance;
+
+        private string connectionString;
+
+        //Paso 2: Redefinir el constructor, para convertirlo en privado
+
+    }
+}
---
+++ Ecommerce/DataAccess/DAO/sqlOperation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace DataAccess.DAO
+{
+    public class sqlOperation
+    {
+        public string ProcedureName { get; set; }
+
+        public List<SqlParameter> Parameters { get; set; }  
+
+        public sqlOperation()
+        {
+            Parameters = new List<SqlParameter>();
+        }
+
+        public void AddStringParam(string paramName, string paramValue) 
+        {
+            Parameters.Add(new SqlParameter(paramName, paramValue));
+        }
+
+        public void AddIntParam(string paramName, int paramValue)
+        {
+            Parameters.Add(new SqlParameter(paramName, paramValue));
+        }
+
+        public void  AddDoubleParam(string paramName, double paramValue)
+        {
+            Parameters.Add(new SqlParameter(paramName, paramValue));
+        } 
+        
+        public void AddDateTimeParam(string paramName, DateTime paramValue)
+        {
+            Parameters.Add(new SqlParameter(paramName, paramValue));
+        }
+    }
+}
