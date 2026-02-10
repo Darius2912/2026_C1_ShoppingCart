3e1e9bfa83721351630de0391a58062b0542ad48
Dario Chaves <DINIA LOPEZ@DESKTOP-OAPB7LG>
2026-02-10 10:35:55 -0600

    inclusion de tablas

---
+++ Ecommerce/tblProduct.sql
@@ -0,0 +1,22 @@
+/****** Object:  Table [dbo].[tblProduct]    Script Date: 10/2/2026 10:34:21 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[tblProduct](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Created] [datetime] NOT NULL,
+	[Updated] [datetime] NULL,
+	[Description] [nvarchar](250) NOT NULL,
+	[Quantity] [int] NOT NULL,
+	[Category] [nvarchar](50) NOT NULL,
+	[Name] [nvarchar](50) NOT NULL,
+	[Price] [decimal](18, 0) NOT NULL,
+ CONSTRAINT [PK_tblProduct] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
---
+++ Ecommerce/tblUser.sql
@@ -0,0 +1,22 @@
+/****** Object:  Table [dbo].[tblUser]    Script Date: 10/2/2026 10:35:02 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[tblUser](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Created] [datetime] NOT NULL,
+	[Updated] [datetime] NULL,
+	[Name] [nvarchar](50) NOT NULL,
+	[LastName] [nvarchar](50) NOT NULL,
+	[Password] [nvarchar](20) NOT NULL,
+	[Email] [nvarchar](50) NOT NULL,
+	[BirthDate] [datetime] NOT NULL,
+ CONSTRAINT [PK_tblUser] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
