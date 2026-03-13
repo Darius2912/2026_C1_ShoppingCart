41ff3db191ce70425516ac64daddd304945d4080
dario <jchavesl@ucenfotec.ac.cr>
2026-03-13 11:50:25 -0600

    SQl

---
+++ Ecommerce/DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Entities_DTOs;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
--- Ecommerce/WebApp/Program.cs
+++ Ecommerce/WebApp/Program.cs
@@ -1,7 +1,16 @@
+using Fluent.Infrastructure.FluentModel;
+using Microsoft.EntityFrameworkCore;
+using ApplicationDbContext = DataAccess.Context.ApplicationDbContext;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+// Agregar DbContext con la cadena de conexión
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
@@ -9,18 +18,14 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapStaticAssets();
-app.MapRazorPages()
-   .WithStaticAssets();
+app.MapRazorPages().WithStaticAssets();
 
 app.Run();
