46763322ee87712d93737248031a316b295d118f
dario <jchavesl@ucenfotec.ac.cr>
2026-03-13 19:29:46 -0600

    sqlDAO

--- Ecommerce/DataAccess/DAO/sqlDAO.cs
+++ Ecommerce/DataAccess/DAO/sqlDAO.cs
@@ -23,7 +23,7 @@ namespace DataAccess.DAO
 
         //Paso 2: Redefinir el constructor, para convertirlo en privado
         private sqlDAO() {
-            connectionString = @"Data Source=ecommercedb.database.windows.net;Initial Catalog=ecommerce-DB;Persist Security Info=True;User ID=jchavesl@ucenfotec.ac.cr;Password=***********;Encrypt=True;Trust Server Certificate=True;Authentication=ActiveDirectoryPassword";
+            connectionString = @"Server=tcp:ecommercedb.database.windows.net,1433;Initial Catalog=ecommerce-DB;Persist Security Info=False;User ID=Darius12;Password=D29mayo@;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
         }
 
 
--- Ecommerce/WebApp/wwwroot/js/ControlActions.js
+++ Ecommerce/WebApp/wwwroot/js/ControlActions.js
@@ -1,6 +1,6 @@
 ﻿function ControlActions() {
 	//Ruta base del API
-	this.URL_API = "https://localhost:7106/api/";
+	this.URL_API = "https://ecommerce-api1-b2gsdec2aebqaxhs.canadacentral-01.azurewebsites.net/api/";
 	//this.URL_API = "https://cenfocinemas-dcordoba-axhnembvfrema9b7.eastus2-01.azurewebsites.net/api/"
 
 	this.GetUrlApiService = function (service) {
