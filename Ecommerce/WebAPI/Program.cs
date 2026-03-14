088e5b890f842d3af8f6eb7563256f09580cb8a9
dario <jchavesl@ucenfotec.ac.cr>
2026-03-13 18:44:23 -0600

    update

--- Ecommerce/WebAPI/Program.cs
+++ Ecommerce/WebAPI/Program.cs
@@ -30,11 +30,11 @@ builder.WebHost.ConfigureKestrel(options =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
